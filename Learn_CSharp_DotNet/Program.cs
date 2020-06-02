c1a0e6f86d52d98a1301cd7d1cd07fe7bf2b97ee
Nguyễn Đình Hiếu <51254371+Hieu-iceTea@users.noreply.github.com>
2020-06-02 12:08:11 +0700

    Creat Project

---
+++ Learn_CSharp_DotNet/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn_CSharp_DotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Hiếu {0}", "Đẹp trai");
+
+            PressEnterToEnd();
+        }
+
+        static void PressEnterToEnd()
+        {
+            Console.WriteLine("Ấn phím [ENTER] để kết thúc");
+            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+        }
+    }
+}
