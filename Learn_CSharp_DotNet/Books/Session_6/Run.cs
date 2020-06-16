0217ea424425c20cff497371a9d4ea348438314a
Nguyễn Đình Hiếu <51254371+Hieu-iceTea@users.noreply.github.com>
2020-06-16 08:34:36 +0700

    update something

--- Learn_CSharp_DotNet/Books/Session_6/Run.cs
+++ Learn_CSharp_DotNet/Books/Session_6/Run.cs
@@ -10,10 +10,34 @@ namespace Learn_CSharp_DotNet.Books.Session_6
     {
         public static void Test()
         {
+            //BaiTap_TrenLop();
+            CodeSnippet_12();
+        }
+
+        private static void BaiTap_TrenLop()
+        {
             var student = new Students();
             student.Display();
 
             student.printName(firstName: "đầu", lastName: "đít");
+        }
+
+        private static void CodeSnippet_12()
+        {
+            int numOne = 10;
+            int numTwo = 20;
+
+            Console.WriteLine("Before: {0} {1}", numOne, numTwo);
+
+            Calculate(ref numOne, ref numTwo);
+
+            Console.WriteLine("After: {0} {1}", numOne, numTwo);
+        }
+
+        private static void Calculate(ref int numOne, ref int numTwo)
+        {
+            numOne *= 2;
+            numTwo /= 2;
         }
     }
 }
