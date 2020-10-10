069eda3c2a4afe24f21d8d69b35b916cf9c9517e
Nguyễn Đình Hiếu <51254371+Hieu-iceTea@users.noreply.github.com>
2020-10-10 09:49:57 +0700

    [Lab 03] Layout

--- Learn_CSharp_UWP/Pages/Lab/Lab_13_Lenendary_Layout_Challenge.xaml.cs
+++ Learn_CSharp_UWP/Pages/Lab/Lab_13_Lenendary_Layout_Challenge.xaml.cs
@@ -13,24 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace Learn_CSharp_UWP
+namespace Learn_CSharp_UWP.Pages.Lab
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Lab_13_Lenendary_Layout_Challenge : Page
     {
-        public MainPage()
+        public Lab_13_Lenendary_Layout_Challenge()
         {
             this.InitializeComponent();
-        }
-
-        private void HyperlinkButton_Click(object sender, RoutedEventArgs e)
-        {
-            //this.Frame.Navigate(typeof(Pages.HelloWorld));
-            this.Frame.Navigate(typeof(Pages.Lab.Lab_08_XAML_Layout_GridAndStackPanel));
         }
     }
 }
